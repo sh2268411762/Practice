d034ebd9a2deb9c1cd177638db16cfe3e8cfc05c
sh2268411762 <2268411762@qq.com>
2020-05-09 01:16:45 +0800

    练习题

---
+++ 牛客练习题/5_8.cpp
@@ -0,0 +1,69 @@
+﻿//#include <stdio.h>
+//#include <string.h>
+//#include <iostream>
+//#define N 5
+//#define M1 N*3
+//
+//#define M2 N*2
+////void main()
+////{
+////	int i;
+////	i = M1 + M2;
+////	printf("%d\n", i);
+////}
+//
+////int main()
+////{
+////	int a = 2;
+////	int b = 3;
+////	int c = 4;
+////	int ret = (a + b) - c + 1 || c + b / 2;
+////	printf("%d\n", ret);
+////	return 0;
+////}
+////
+////long fun(char str[])
+////{
+////	int i;
+////	long s = 0, t;
+////	for (i = 0; str[i] != '\0'; i++)
+////	{
+////		t = 0;
+////		while (str[i] >= '0' && str[i] <= '9')
+////		{
+////			t = t * 10 + str[i] - '0';
+////			i++;
+////		}
+////		s = s + t;
+////	}
+////	return s;
+////}
+////
+////void main()
+////{
+////	char string[] = "Welcome123To456DuoYi789!";
+////	long len;
+////	len = fun(string);
+////	printf("%ld\n", len);
+////}
+//
+//
+//int main()
+//{
+//	//int m, n, p;
+//	//scanf("m=%dn=%dp=%d", &m, &n, &p);
+//	//printf("%d%d%d\n", m, n, p);
+//
+//	union Data
+//	{
+//		struct { int x; int y; }s;
+//		int x, y;
+//	}d;
+//
+//	d.x = 1;
+//	d.y = 2;
+//	d.s.x = d.x * d.x;
+//	d.s.y = d.y + d.y;
+//	printf("%d %d\n", d.s.x, d.s.y);
+//}
+//
