1e7095c3b31471ac02b9f46eabe98e247daaa1b2
sh2268411762 <2268411762@qq.com>
2020-05-09 19:19:19 +0800

    练习

---
+++ 牛客练习题/5.9.c
@@ -0,0 +1,31 @@
+//#include <stdio.h>
+////C语言主要是借助定义函数的方式来实现程序模块化
+//
+//void fun(char* p, int n);
+//int main()
+//{
+//	//double a = 5.5;
+//	//double b = 2.5;
+//	//int ret = (int)a + b / b;
+//	//printf("%d\n", ret);
+//
+//	//char a;
+//	//int b;
+//	//float c;
+//	//scanf("%c,b=%d,f=:%f", &a, &b, &c);
+//
+//	char a[6] = "ABCDE";
+//	fun(a, 5);
+//	printf("%s\n", a);
+//	return 0;
+//}
+//
+//void fun(char* p, int n)//与a无关
+//{
+//	char b[6] = "abcde";
+//	int i;
+//	for (i = 0, p = b; i < n; i++)
+//	{
+//		p[i] = b[i];
+//	}
+//}
