af67cd9b5f198a3dbaa1bf17ca4fe12f7d85c623
sh2268411762 <2268411762@qq.com>
2020-05-10 23:37:15 +0800

    Practice

---
+++ 牛客练习题/5.10.cpp
@@ -0,0 +1,56 @@
+//#include <stdio.h>
+//#include <iostream>
+//
+////using namespace std;
+////class A {
+////public:
+////    virtual void f() { cout << "A::f() "; }
+////    void f() const { cout << "A::f() const "; }
+////};
+////class B : public A {
+////public:
+////    void f() { cout << "B::f() "; }
+////    void f() const { cout << "B::f() const "; }
+////};
+////void g(const A* a) {
+////    a->f();
+////}
+////int main(int argc, char* argv[]) {
+////    A* p = new B();
+////    p->f();
+////    g(p);
+////    delete(p);
+////    return 0;
+////}
+//
+//
+//int main()
+//{
+//	//int i, j, k, a = 3, b = 2;
+//	//i = (--a == b++) ? --a : ++b;
+//	//j = a++;
+//	//k = b;
+//	//printf("i = %d\n,j = %d\n,k = %d\n", i, j, k);
+//
+//	//int a = 2, b = 1, c = 2;
+//	//if (a < b)
+//	//if (b < 0)
+//	//c = 0;
+//	//else
+//	//c += 1;
+//	//printf("%d\n", c);
+//
+//	//int a = 0, b = 0, c = 0, d;
+//	////c = (a + = b, , b + = a);//´íÎó
+//	//d = c;
+//	//;
+//	//; printf("%d,%d,%d\n",a, b, c);
+//
+//	//int c = '\0';
+//
+//	//int a, b;
+//	//a = 32767;
+//	//b = a + 1;
+//	//printf("a = %d\nb = %d\n", a, b);
+//	return 0;
+//}
