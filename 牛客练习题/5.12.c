f2ce12850243c7f4ca072ff4182db0cb196265f7
sh2268411762 <2268411762@qq.com>
2020-05-12 20:53:43 +0800

    练习题

---
+++ 牛客练习题/5.12.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char c;
+//	int i;
+//	long int l;
+//	float f;
+//	double d;
+//	printf("%d", sizeof(c));
+//	printf("%d", sizeof(i));
+//	printf("%d", sizeof(l));
+//	printf("%d", sizeof(f));
+//	printf("%d", sizeof(d));
+//
+//	return 0;
+//}
