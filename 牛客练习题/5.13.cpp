c24ef7eea8bf0e4ddb1eb9c0266e51cbf46f1a70
sh2268411762 <2268411762@qq.com>
2020-05-13 22:25:22 +0800

    练习

---
+++ 牛客练习题/5.13.cpp
@@ -0,0 +1,54 @@
+//#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+//#define M(x,y,z) x*y+z
+//
+//class A {
+//
+//};
+//class B {
+//	char ch;
+//	int x;
+//};
+//class C{
+//public:
+//	void Print(void){}
+//};
+//class D {
+//public:
+//	virtual void Print(void){}
+//};
+//int main()
+//{
+//	//printf("%d\n%d\n%d\n%d\n", sizeof(A), sizeof(B), sizeof(C), sizeof(D));
+//
+//	//char* p1 = "hello";
+//	//char* p2 = "world";
+//	//char* p3 = "a piece";
+//	//char* str[] = { p1,p2,p3 };
+//	//printf("%c", *(str[0] + 1));
+//
+//	//int i = 3, j = 3;
+//	//j = i++;
+//	//j++;
+//	//++i;
+//
+//	float k = 0.8567;
+//	printf("%06.1f%%", k * 100);
+//
+//
+//	//int arr[5];
+//	//int aRR(5);
+//	//int* p = new int[5];
+//
+//	//int a = 10;
+//	//short s = 0;
+//	//printf("%d\n%d\n\n", sizeof(a), sizeof(s));
+//	//printf("%d\n%d\n\n", sizeof(int), sizeof(short));
+//
+//	//int a = 1, b = 2, c = 3;
+//	//int ret = M(a + b, b + c, c + a);
+//	//int RET = M(3, 5, 4);
+//	//printf("%d\n", ret);
+//	return 0;
+//}
