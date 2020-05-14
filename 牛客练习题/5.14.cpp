41cc5653eca70049cb5b52429ff84096408363e6
sh2268411762 <2268411762@qq.com>
2020-05-14 20:42:56 +0800

    练习

---
+++ 牛客练习题/5.14.cpp
@@ -0,0 +1,61 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char x = 040;
+//	printf("%o\n", x << 1);
+//
+//
+//	//int s = 0, n;
+//	//for (n = 0; n < 4; n++)
+//	//{
+//	//	switch (n)
+//	//	{
+//	//	default:
+//	//		s += 4;
+//	//	case 1:
+//	//		s += 1;
+//	//	case 2:
+//	//		s += 2;
+//	//	case 3:
+//	//		s += 3;
+//	//	}
+//	//}
+//	//printf("%d\n", s);
+//	return 0;
+//}
+//
+//
+////#include <iostream>
+//
+////using namespace std;
+////
+////class cla {
+////	static int n;
+////	public:
+////	 cla() { n++; }
+////	~cla() { n--; }
+////	static int get_n() { return n; }
+////};
+////int main()
+////{
+////	cla *p = new cla;
+////	delete p;
+////	cout << "n = " << cla::get_n() << endl;
+////	return 0;
+////}
+//
+//
+////int main()
+////{
+////	int p[7] = { 11,13,14,15,16,17,18 };
+////	int i = 0;
+////	int j = 0;
+////
+////	while (i < 7 && 1 == p[i] % 2)
+////	{
+////		j += p[i++];
+////	}
+////	printf("%d\n", j);
+////	return 0;
+////}
