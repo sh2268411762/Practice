52f0f7e512023f1e0a3dd36d2255399f91f30cc3
sh2268411762 <2268411762@qq.com>
2020-05-18 23:56:55 +0800

    练习

--- 牛客练习题/5.16.cpp
+++ 牛客练习题/5.16.cpp
@@ -1,39 +1,39 @@
-#include <stdio.h>
-
-#define MA(x,y) (x*y)
-int main()
-{
-	//int a[10] = { 0,1,2,3,4,5 };
-
-	//double d = 3.678509;
-	//printf("%-30.4e\n", d);
-	//printf("%4.30e\n", d);
-	//printf("%-30.4f\n", d);
-	//printf("%-4.30e\n", d);
-
-	//int i = 5;
-	//i = MA(i, i + 1) - 7;
-	//printf("%d\n", i);
-
-	//int a[2][2][3] = { {{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}} };
-	//int* p = (int*)(&a + 1);//  &a+1 = a + i * sizeof(a)
-	//printf("%d  %d", *(int*)(a + 1), *(p - 1));
-
-	//int i = 0;
-	//int count = 0;
-	//while (i < 10)
-	//{
-	//	if (i < 1)
-	//	{
-	//		continue;
-	//	}
-	//	if (5 == i)
-	//	{
-	//		break;
-	//	}
-	//	i++;
-	//	count++;
-	//}
-	//printf("%d", count);   //ËÀÑ­»·
-	return 0;
-}
+//#include <stdio.h>
+//
+//#define MA(x,y) (x*y)
+//int main()
+//{
+//	//int a[10] = { 0,1,2,3,4,5 };
+//
+//	//double d = 3.678509;
+//	//printf("%-30.4e\n", d);
+//	//printf("%4.30e\n", d);
+//	//printf("%-30.4f\n", d);
+//	//printf("%-4.30e\n", d);
+//
+//	//int i = 5;
+//	//i = MA(i, i + 1) - 7;
+//	//printf("%d\n", i);
+//
+//	//int a[2][2][3] = { {{1,2,3},{4,5,6}},{{7,8,9},{10,11,12}} };
+//	//int* p = (int*)(&a + 1);//  &a+1 = a + i * sizeof(a)
+//	//printf("%d  %d", *(int*)(a + 1), *(p - 1));
+//
+//	//int i = 0;
+//	//int count = 0;
+//	//while (i < 10)
+//	//{
+//	//	if (i < 1)
+//	//	{
+//	//		continue;
+//	//	}
+//	//	if (5 == i)
+//	//	{
+//	//		break;
+//	//	}
+//	//	i++;
+//	//	count++;
+//	//}
+//	//printf("%d", count);   //ËÀÑ­»·
+//	return 0;
+//}
---
+++ 牛客练习题/5.18.cpp
@@ -0,0 +1,41 @@
+//#include <stdio.h>
+//#include <string.h>
+//#include <iostream>
+//
+//using namespace std;
+//
+////char* fun(char* t)
+////{
+////	char* p = t;
+////	return (p + strlen(t) / 2);
+////}
+//
+//public static int func()
+//{
+//	try
+//	{
+//		return 1;
+//	}
+//	catch (Exception e)
+//	{
+//		return 2;
+//	}
+//	finally
+//	{
+//		return 3;
+//	}
+//}
+//
+//int main()
+//{
+//	//char* str = "abcdefgh";
+//	//str = fun(str);
+//	//puts(str);
+//
+//	//char s[6] = { 'x','y','z','\0','1','2' };
+//	//puts(s);
+//
+//	int ret = func();
+//	printf("%d", ret);
+//	return 0;
+//}
