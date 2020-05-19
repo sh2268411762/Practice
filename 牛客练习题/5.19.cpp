3f67fdf9fc37a214236b889728b650941de98951
sh2268411762 <2268411762@qq.com>
2020-05-19 23:45:48 +0800

    练习题

---
+++ 牛客练习题/5.19.cpp
@@ -0,0 +1,41 @@
+//#include <stdio.h>
+//
+//#define a 10
+////void main()
+////{
+////	printf("%d", 150 / 50 % 5);
+////}
+//
+//int fun(int x[], int k)
+//{
+//	if (0 == k)
+//	{
+//		return (x[0]);
+//	}
+//	return x[0] + fun(x + 1, k - 1);
+//}
+//void foo()
+//{
+//#undef a
+//#define a 50
+//}
+//int main()
+//{
+//	//const int i = 0;
+//	//int* j = (int*)&i;
+//	//*j = 1;//1
+//	//printf("%d %d", i, *j);
+//
+//	//int a[5] = { 1,2,3,4,5 };
+//	//int* pp = (int*)(&a + 1);
+//	//printf("%d,%d", *(a + 1), *(pp - 1));
+//
+//	//int x[] = { 1,2,3,4,5 };
+//	//int ret = fun(x, 3);
+//	//printf("%d\n", ret);
+//
+//	printf("%d..", a);
+//	foo();
+//	printf("%d", a);
+//	return 0;
+//}
