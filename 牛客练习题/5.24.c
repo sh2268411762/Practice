e34d4102a039919b772c5daf064d47b4664dc492
sh2268411762 <2268411762@qq.com>
2020-05-25 01:04:16 +0800

    5.24

---
+++ 牛客练习题/5.24.c
@@ -0,0 +1,47 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//
+//int main()
+//{
+//	//int a, b;
+//	//for (a = 0, b = 0; b != 100 && a < 5; a++)
+//	//{
+//	//	scanf("%d", &b);
+//	//}
+//
+//
+//	//int a[12] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
+//	//int* p[4];
+//	//int i;
+//	//for (i = 0; i < 4; i++)
+//	//{
+//	//	p[i] = &a[i * 3];
+//	//}
+//	//printf("%d\n", p[3][2]);
+//
+//
+//	//char a[5][10] = { "one","two","three","four","five" };
+//	//int i, j;
+//	//char t;
+//	//for (i = 0; i < 2; i++)
+//	//{
+//	//	for (j = i + 1; j < 5; j++)
+//	//	{
+//	//		if (a[i][0] > a[j][0])
+//	//		{
+//	//			t = a[i][0];
+//	//			a[i][0] = a[j][0];
+//	//			a[j][0] = t;
+//	//		}
+//	//	}
+//	//}
+//	//puts(a[1]);
+//
+//
+//	char ch = 'a';
+//	int k = 12;
+//	printf("%x,%o,", ch, ch, k);
+//	printf("k=%%d\n", k);
+//	return EXIT_SUCCESS;
+//}
