70730f75d85b095a26b41b3300d9e0605c443e6b
sh2268411762 <2268411762@qq.com>
2020-05-25 23:24:32 +0800

    练习

---
+++ 牛客练习题/5.25.cpp
@@ -0,0 +1,30 @@
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+////	char c = "500";
+////	char d = '500';
+////	printf("%c  %c", c, d);
+//
+//	int x = 3, y = 4, z = 5;
+//	int ret = !(x + y) + z - 1 && y + z / 2;
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//
+////#include<iostream>
+////using namespace std;
+////class TestClass {
+////	char x;
+////public:
+////	TestClass() { cout << 'A'; }
+////	TestClass(char c) { cout << c; }
+////	~TestClass() { cout << 'B'; }
+////};
+////int main() {
+////	TestClass p1, * p2;
+////	p2 = new TestClass('X');
+////	delete p2;
+////	return 0;
+////}
