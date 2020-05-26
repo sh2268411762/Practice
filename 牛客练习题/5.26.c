e4c9858aa39a1bd20001cf0834a4a5ab6be56a52
sh2268411762 <2268411762@qq.com>
2020-05-26 10:58:10 +0800

    练习

---
+++ 牛客练习题/5.26.c
@@ -0,0 +1,86 @@
+//#include <stdio.h>
+////#include <cstdint> 
+////#define S(a,b) t=a;a=b;b=t
+//
+//int main()
+//{
+//	//int t = 10;
+//	//int a = 80;
+//	//int b = 7;
+//	//S(a, b);
+//	//printf("%d %d", a, b);
+//
+//
+////	int i;
+////	for (i = 0; i < 2; i++)
+////	{
+//////		fork();
+////		printf("-");
+////	}
+//
+//	float a;
+//	unsigned int b;
+//	//scanf("%6.2f%d", &a, &b);
+//	scanf("%f%n", &a, &b);
+//	//scanf("%f%3o", &a, &b);
+//
+//	return 0;
+//}
+//
+////int main()
+////{
+////	uint32_t a = 100;
+////	while (a > 0)
+////	{
+////		--a;
+////	}
+////	printf("%d", a);
+////	return 0;
+////}
+//
+//
+////char f(char x)
+////{
+////	return x * x + 'a';
+////}
+////int main()
+////{
+////	char a, b = 0;
+////	for (a = 0; a < 4; a += 1)
+////	{
+////		b = f(a);
+////		putchar(b);
+////	}
+////	return 0;
+////}
+////
+////int main()
+////{
+////	int x = 1, y = 0, a = 0, b = 0;
+////	switch (x)
+////	{
+////	case 1:
+////		switch (y)
+////		{
+////		case 0:
+////			a++;
+////			break;
+////
+////		case 1:
+////			b++;
+////			break;
+////		}
+////	case 2:
+////		a++;
+////		b++;
+////		break;
+////
+////	case 3:
+////		a++;
+////		b++;
+////		break;
+////	}
+////	printf("%d  %d ", a, b);
+////	return 0;
+////}
+//
