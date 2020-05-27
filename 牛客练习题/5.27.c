9f1a911aa7b138c56fdf50ba2450793c0d51aeae
sh2268411762 <2268411762@qq.com>
2020-05-27 17:08:41 +0800

    练习题

---
+++ 牛客练习题/5.27.c
@@ -0,0 +1,35 @@
+//#include <stdio.h>
+//#define ADD(x) x*x
+//
+//int main()
+//{
+//	//int a = 4, b = 6, c = 7;
+//	//int d = ADD(a + b) * c;
+//	//printf("%d", d);
+//
+//
+//	//int a = 10;
+//	//double b = 3.14;
+//
+//
+//	//char str[] = "Computer";
+//	//char* p;
+//	//p = str;
+//	//printf("%s\n", p + 5);
+//
+//
+//	int i = 1;
+//	int j = i++;
+//	if (i++ > ++j)
+//	{
+//		if (++i == j)
+//		{
+//			i += j;
+//		}
+//	}
+//	printf("%d\n", i);
+//
+//
+//	int a[10];
+//	return 0;
+//}
