c9989908bc70043982487de4c1bf7af1550a0483
sh2268411762 <2268411762@qq.com>
2020-06-01 23:06:18 +0800

    6.1

---
+++ 牛客练习题/6.1.c
@@ -0,0 +1,45 @@
+//#include <stdio.h>
+//
+//
+//
+//char* getMem()
+//{
+//	char p[] = "hello world ";
+//	p[5] = 0x0;
+//	return p;
+//}
+//void test()
+//{
+//	char* s = 0x0;
+//	s = getMem();
+//	printf(s);
+//}
+//int main()
+//{
+//	//int sum, pad, pAd;
+//	//sum = pad = 5;
+//	//pAd = ++sum, pAd++, ++pad;
+//	//printf("%d\n", pad);
+//
+//
+//	//int x, y;
+//	//x = y = 1;
+//	//++x || ++y;
+//	//printf("%d\n", y);
+//
+//
+//	//test();
+//
+//
+//	double a = 9.0;
+//	double b = 8.9;
+//	int w = 24;
+//	long c = 89;
+//
+////	a = a + b = b++;
+//	w % (int)a + b;
+//	(c + w) % (int)a;
+//	w = a == b;
+//
+//	return 0;
+//}
