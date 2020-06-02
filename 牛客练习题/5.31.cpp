78b036b929477d9effcfbc5dac92952e0761a39c
sh2268411762 <2268411762@qq.com>
2020-06-02 23:43:32 +0800

    练习

---
+++ 牛客练习题/5.31.cpp
@@ -0,0 +1,87 @@
+////#include <stdio.h>
+////#include <stdlib.h>
+////#include <string.h>
+//#include <iostream>
+//using namespace std;
+////
+////
+////void main()
+////{
+////	int a, x;
+////	for (a = 0, x = 0; a <= 1 && !x++; a++)
+////	{
+////		a++;
+////	}
+////	cout << a << x << endl;
+////
+////}
+//
+//#pragma pack(2)
+//struct A
+//{
+//	int a;
+//	char b;
+//	short c;
+//};
+//#pragma pack()
+//
+//#pragma pack(4)
+//struct B
+//{
+//	char b;
+//	int a;
+//	short c;
+//};
+//#pragma pack()
+//int count = 0;
+//int x(int n)
+//{
+////	count++;
+////	cnt++;
+//	if (n <= 3)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return x(n - 2) + x(n - 4) + 1;
+//	}
+//}
+//int main()
+//{
+//	//struct A a;
+//	//struct B b;
+//	//printf("%d  %d", sizeof(a), sizeof(b));
+//
+//
+//	//int x = 6;
+//	//int y = 3;
+//	//x *= y + 8;
+//	//printf("%d\n", x);
+//	//x = 6;
+//	//x = x * y + 8;
+//	//printf("%d\n", x);
+//	//x = 6;
+//	//x = x * (y + 8);
+//	//printf("%d\n", x);
+//
+//
+//	//x(x(8));
+//	//printf("%d\n", count++);
+//
+//
+//	char str1[] = "hello world";
+//	char str2[] = "hello world";
+//
+//	const char str3[] = "hello world";
+//	const char str4[] = "hello world";
+//
+//	const char* p1 = "hello world";
+//	const char* p2 = "hello world";
+//
+//	cout << boolalpha << (str1 == str2) << ',';
+//	cout << boolalpha << (str3 == str4) << ',';
+//	cout << boolalpha << (p1 == p2) << endl;
+//	return 0;
+//}
+//
---
+++ 牛客练习题/6.2.c
@@ -0,0 +1,59 @@
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+////#include <iostream>
+////using namespace std;
+//
+//
+//int f(int x, int y)
+//{
+//	return (x + y);
+//}
+//
+//void fun(int* x)
+//{
+//	printf("%x\n", ++ * x);
+//}
+//int main()
+//{
+//	//int i = 10, j = 0;
+//	//do {
+//	//	j = j + i;
+//	//	i--;
+//	//} while (i > 2);
+//	//printf("%d", j);
+//
+//
+//	//int i = 9;
+//	//printf("%o\n", i);
+//
+//
+//	//int i, x[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	//for (i = 0; i < 3; i++)
+//	//{
+//	//	printf(" %d ", x[i][2 - i]);
+//	//}
+//	//puts(" ");
+//
+//
+//	//int i = 100;
+//	//cout.unsetf(ios::dec);
+//	//cout.setf(ios::hex);
+//	//cout << i << "\t";
+//	//cout << i << "\t";
+//	//cout.setf(ios::dec);
+//	//cout << i << "\t";
+//
+//
+//	//int a = 1, b = 2, c = 3, d = 4, e = 5;
+//	//printf("%d \n", f((a + b, b + c, c + a), (d, e)));
+//
+//
+//	//int a = 25;
+//	//fun(&a);
+//
+//
+//	char s[] = "\\123456\123456\t";
+//	printf("%d\n", strlen(s));
+//	return 0;
+//}
