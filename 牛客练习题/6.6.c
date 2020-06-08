8d51baac86fcf9cc482f8a7575603399772ddcbf
sh2268411762 <2268411762@qq.com>
2020-06-08 23:13:23 +0800

    练习

---
+++ 牛客练习题/6.6.c
@@ -0,0 +1,66 @@
+//#include <stdio.h>
+//#define N 2
+//#define M N+1
+//#define NUM (M+1)*M/2
+//#include <stdlib.h>
+//int main()
+//{
+//	//int a = 5, b = 10, c;
+//	//int* p1 = &a;
+//	//int* p2 = &b;
+//
+//
+//	//char s[20] = "proydguGyfdyufu";
+//	//char* ps = s;
+//	//char s1 = ps + 2;
+//	//char s2 = s[2];
+//	//char s3 = ps[2];
+//	//char s4 = *(ps + 2);
+//
+//
+//
+//	//printf("%d\n", NUM);
+//
+//
+//	//int a[3][2] = { 0 };
+//	//int(*p)[2];
+//	//int i, j;
+//	//for (i = 0; i < 2; i++)
+//	//{
+//	//	p = a + i;
+//	//	scanf("%d", p);
+//	//	p++;
+//	//}
+//	//for (i = 0; i < 3; i++)
+//	//{
+//	//	for (j = 0; j < 2; j++)
+//	//	{
+//	//		printf("%2d", a[i][j]);
+//	//	}
+//	//	printf("\n");
+//	//}
+//
+//
+//
+//
+//	return 0;
+//}
+
+
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	bool v0 = 1 * 0 && 2 * 2;
+//	bool v1 = 2 * 0 || 2 * 2;
+//
+//	printf("Hello");
+//	fork();
+//	printf("Hello");
+//
+//	return 0;
+//}
