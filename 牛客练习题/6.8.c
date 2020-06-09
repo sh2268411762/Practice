e7a079d646e7cc6d95aa768d89a602dc91f3d6fb
sh2268411762 <2268411762@qq.com>
2020-06-09 23:37:58 +0800

    6.8练习

---
+++ 牛客练习题/6.8.c
@@ -0,0 +1,37 @@
+//#include <stdio.h>
+//
+//
+//
+//
+//int* fun(int *i, int* j)
+//{
+//	return ((*i > * j) ? i : j);
+//}
+//int main()
+//{
+//	//int i;
+//	//for (i = 0; i < 3; i++)
+//	//{
+//	//	switch (i)
+//	//	{
+//	//	case 0:
+//	//		printf("%d", i);
+//	//	case 2:
+//	//		printf("%d", i);
+//	//	default:
+//	//		printf("%d", i);
+//	//	}
+//	//}
+//
+//
+//
+//
+//	int a, b;
+//	scanf("%d,%d", &a, &b);
+//	int* k = &a;//
+//	int* t = &b;
+//
+//	k = fun(k, t);
+//	printf("%d\n", *k);
+//	return 0;
+//}
