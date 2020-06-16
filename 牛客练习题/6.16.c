6e5e1786d45cc0902b9aaacdc16ded22be6f710e
sh2268411762 <2268411762@qq.com>
2020-06-16 23:21:39 +0800

    6.16

---
+++ 牛客练习题/6.16.c
@@ -0,0 +1,49 @@
+//#include <stdio.h>
+//
+//enum weekday {
+//	sun,
+//	mon=3,
+//	tue,//4
+//	wed,//5
+//	thu//6
+//};
+//int main()
+//{
+//	//int a = 14, b = 15, x;
+//	//char c = 'A';
+//	//x = (a && b) && (c < 'B');
+//	//printf("%d\n", x);
+//
+//
+//
+//	//enum weekday workday;
+//	//workday = wed;
+//	//printf("%d\n", workday);
+//
+//
+//
+//	//int c = 0, k;
+//	//for (k = 1; k < 3; k++)
+//	//{
+//	//	switch (k)
+//	//	{
+//	//	default:c += k;
+//
+//	//	case 2:c++;
+//	//		break;
+//
+//	//	case 4:c += 2;
+//	//		break;
+//	//	}
+//	//}
+//	//printf("%d\n", c);
+//
+//
+//
+//	//int A[2][2] = { 1,2,3,4 };
+//	//int* p = A;
+//	//p = &A[0][0];
+//	//p = A[0][0];
+//	//p = A[0];
+//	return 0;
+//}
