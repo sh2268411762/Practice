b2803e8b0fc902974303f6b0691d4b8deb5c55e5
sh2268411762 <2268411762@qq.com>
2020-06-17 18:46:39 +0800

    6.17

---
+++ 牛客练习题/6.15.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//
+//int f(int a, int b, int c) {
+//	return 0;
+//}
+//int main()
+//{
+//	//char s[] = "abcdef";
+//	//printf("%s\n", s);
+//
+//
+//	return f(printf("a"), printf("b"), printf("c"));
+//	return 0;
+//}
---
+++ 牛客练习题/6.17.c
@@ -0,0 +1,54 @@
+//#include <stdio.h>
+//
+//static int a = 1;
+//void f1()
+//{
+//	a = 2;
+//}
+//void f2()
+//{
+//	int a = 3;
+//}
+//void f3()
+//{
+//	static int a = 4;
+//}
+//int main()
+//{
+//	//int a = 1, b = 2, c = 3, d = 4;
+//	//a = (b = 4) + (c = 6);
+//	//printf("%d", a);
+//
+//
+//
+//	//int a[3][3], * p, i;
+//	//p = &a[0][0];
+//	//for (i = 0; i < 9; i++)
+//	//{
+//	//	p[i] = i;
+//	//}
+//	//for (i = 0; i < 3; i++)
+//	//{
+//	//	printf("%d  ", a[1][i]);
+//	//}
+//
+//
+//
+//	//char a = 'A', b = 'Y';
+//	//printf("%d,%d", a, b);
+//
+//
+//	//char** s;
+//	//char* str = "data";
+//	//s = &str;
+//
+//
+//	//printf("%d", a);
+//	//f1();
+//	//printf("%d", a);
+//	//f2();
+//	//printf("%d", a);
+//	//f3();
+//	//printf("%d", a);
+//	return 0;
+//}
