786a103cdfd84781e6b9a0f7e31f78b04ae86f42
sh2268411762 <2268411762@qq.com>
2020-06-18 23:51:00 +0800

    6.18

---
+++ 牛客练习题/6.9.c
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+//
+//#define S345
+//int main()
+//{
+//	//int p[][4] = { {1},{3,2},{4,5,6},{0} };
+//
+//	//printf("%d,%d\n", sizeof(long), sizeof(int));
+//	//printf("%d\n", sizeof(unsigned int));
+//
+//
+//	//int a, b, c;
+//	//a = 2;
+//	//b = 9;
+//	//a++;//3
+//	//c = --b;//c = 8  ,b = 8
+//	//a += b - c;//3
+//	//printf("%d\n", a);
+//
+//
+//
+//	//char ch = 'a';
+//	//int k = 12;
+//	//printf("%x,%o\n", ch, ch, k);
+//	//printf("k=%%d", k);
+//	return 0;
+//}
