59009e054f95862629c647dd5c74fedbdec42071
sh2268411762 <2268411762@qq.com>
2020-10-19 23:58:23 +0800

    10.19牛客习题

---
+++ 牛客/牛客/10.19.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+
+////输入3科成绩，然后把三科成绩输出，成绩为整数形式。
+//int main()
+//{
+//    int num1 = 0, num2 = 0, num3 = 0;
+//    cin >> num1 >> num2 >> num3;
+//    cout << "score1=" << num1 << ",score2=" << num2 << ",score3=" << num3 << endl;
+//    return 0;
+//}
+
+
+////包括两行：
+////第一行为“Hello world!”
+////第二行为printf(“Hello world!”)调用后的返回值。
+//int main()
+//{
+//	cout << printf("\"Hello world!\"\n");
+//	return 0;
+//}
+
+
+////十六进制整数ABCDEF对应的十进制整数，所占域宽为15。
+//int main()
+//{
+//	printf("%15d", 0xABCDEF);
+//	return 0;
+//}
+
+
+
+
+////十进制整数1234对应的八进制和十六进制（字母大写），用空格分开，
+////并且要求，在八进制前显示前导0，在十六进制数前显示前导0X。
+//int main()
+//{
+//    printf("%#o %#X\n", 1234, 1234);
+//}
+
+
+////KiKi学会了printf在屏幕输出信息，他想输出一架小飞机。请帮他编写程序输出这架小飞机。
+//int main()
+//{
+//    for (int i = 0; i < 12; i++)
+//    {
+//        switch (i)
+//        {
+//        case 1:
+//        case 3:
+//            cout << "     **" << endl;
+//            break;
+//        case 5:
+//        case 7:
+//            cout << "************" << endl;
+//            break;
+//        case 9:
+//        case 11:
+//            cout << "    *  *" << endl;
+//            break;
+//        default:
+//            cout;
+//            break;
+//        }
+//    }
+//    return 0;
+//}
